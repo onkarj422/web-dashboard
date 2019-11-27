f91e6d0141691d8e83466af41bcccd364bfbb8c4
Onkar Joshi <onkar.joshi@knorex.com>
2019-11-27 22:41:56 +0530

    final update webpack package.json

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,7 +12,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/webdashboard',
+      publicPath: '/web-dashboard/',
     },
     options.output,
   ), // Merge with env dependent settings
